9332ab65dc2de5132b0e2121b38f105ba03effab
TitoLito22 <taiong2016@gmail.com>
2024-01-24 09:28:28 -0500

    Updated battery, car, engine with new python files

--- car.py
+++ car.py
@@ -2,8 +2,11 @@ from abc import ABC, abstractmethod
 
 
 class Car(ABC):
-    def __init__(self, last_service_date):
+    def __init__(self, last_service_date, current_service_date, engine, battery):
         self.last_service_date = last_service_date
+        self.current_service_date = current_service_date
+        self.engine = engine
+        self.battery = battery
 
     @abstractmethod
     def needs_service(self):
