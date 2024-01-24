8f189ecc2a8ea00c5d06be334b86e944074edab1
TitoLito22 <taiong2016@gmail.com>
2024-01-24 09:22:12 -0500

    Added Battery

---
+++ engine/Battery/spindler_battery.py
@@ -0,0 +1,12 @@
+from abc import ABC
+
+from car import Car
+
+
+class SpindlerBattery(Car, ABC):
+    def __init__(self, last_service_date, current_service_date):
+        self.last_service_date = last_service_date
+        self.current_service_date = current_service_date
+
+    def engine_should_be_serviced(self):
+        return self.current_service_date - self.last_service_date > 2
